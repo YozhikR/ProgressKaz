754abd56f708795f5442abf891e3c41d7bcc10b8
Sanzhar <thesanjik@gmail.com>
2020-06-11 03:48:04 +0600

    service

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ let paths = {
 	scripts: {
 		src: [
 			'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+			'node_modules/owl.carousel/dist/owl.carousel.min.js',
 			baseDir + '/js/app.js' // app.js. Always at the end
 		],
 		dest: baseDir + '/js',
