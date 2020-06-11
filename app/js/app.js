a9b7628fc03fc3dbc3ea91531044b64e31c48db0
Yozhik333 <44803662+Yozhik333@users.noreply.github.com>
2020-06-11 09:10:29 +0600

    download-info.html и незаконченный contacts.html (Осталась карта)

--- app/js/app.js
+++ app/js/app.js
@@ -27,6 +27,11 @@ $('#inner3-carousel').owlCarousel({
 	items: 2
 })
 
-document.addEventListener("DOMContentLoaded", function() {
 
-});
+// Tabs dowload-info
+
+$(".tab_item").not(":first").hide();
+$(".download-info-tabs-wrapper .tab").click(function () {
+	$(".download-info-tabs-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
